dd0984e40f71153a4e53a453298d82b7d1ea6bca
yoj9168@gmail.com <yoj9168@gmail.com>
2023-01-02 23:24:26 +0900

    ADD: 회원정보 수정 테스트코드 완성

--- src/main/java/com/today_diary/admin/web/dto/UserUpdateRequestDto.java
+++ src/main/java/com/today_diary/admin/web/dto/UserUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.today_diary.admin.web.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,9 +11,12 @@ public class UserUpdateRequestDto {
     private String sex;
     private int age;
 
+    @Builder
     public UserUpdateRequestDto(String phoneNumber, String sex, int age){
         this.phoneNumber = phoneNumber;
         this.sex = sex;
         this.age = age;
     }
+
+
 }
